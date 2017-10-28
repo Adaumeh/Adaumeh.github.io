0850f44ea286d81d493b7cfbf0cf47cab683085e
umeononihu adaobi <adaumeh2003@yahoo.com>
2017-10-28 21:04:44 +0100

    just got my update book route working

--- server/controllers/putbooks.js
+++ server/controllers/putbooks.js
@@ -4,20 +4,16 @@ module.exports = {
 
 update(req, res) {
   return putbookscontroller
-    .findAll()
-    .then(putbookscontroller => res.status(200).send(putbookscontroller))
-    .catch(error => res.status(400).send(error));
-    return putbookscontroller
-    .then(putbookscontroller=> {
+    .findById(req.params.bookId) 
+       .then(putbookscontroller=> {
       if (!putbookscontroller) {
         return res.status(404).send({
           message: 'books Not Found',
         });
       }
       return putbookscontroller
-        .updateAttributes({
-         title: req.body.title || req.body.year || req.body.author,
-
+        .update({
+          title: req.body.title || req.body.author||req.body.year
         })
         .then(() => res.status(200).send(putbookscontroller))  // Send back the updated todo.
         .catch((error) => res.status(400).send(error));
