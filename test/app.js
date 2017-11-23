12438d5a8e22e00178aec314dee4f13514b4dfd8
umeononihu adaobi <adaumeh2003@yahoo.com>
2017-11-23 22:59:50 +0100

    writing my unit test

--- test/app.js
+++ test/app.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const http = require('http');
 
+const express = require('express');
 const logger = require('morgan');
 const jwt = require('jsonwebtoken');
-const secret= "drtguug8*werty+uifghyu";
+const secret= "drtguug8*werty+uifghyu"
 const bodyParser = require('body-parser');
-
-//const bcrypt = require('bcrypt');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const Sequelize = require('sequelize');
 // Set up the express app
 const app = express();
 // Log requests to the console.
-const server = http.createServer(app)
+const http = require('http');
+const port = process.env.PORT || 8000;
+if (process.env.NODE_ENV !== 'production') {
+require('dotenv').config()
+}
+//const server = http.createServer(app);
+//var Console = console;
+//
 app.set('superSecret', secret)
 app.use(logger('dev'));
-
+app.use(session({
+    secret: 'drtguug8*werty+uifghyu',
+    resave: true,
+    saveUninitialized: true
+}));
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
@@ -23,4 +34,5 @@ require('../server/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
+
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,40 +1,80 @@
-/*process.env.NODE_ENV = 'test';
-//let sequelize = require("sequelize")
-let supertest = require("supertest");
-let Books = require('../server/models/books');
-let addbooks = require('../server/controllers/addbooks')
-let getbooks = require('../server/controllers/getbooks')
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-//let server = require('../server');
-let should = chai.should();
+'use strict';
+ 
+const chai = require('chai');  
+const expect = require('chai').expect;
+ const users = require('../server/controllers/users')
 
-chai.use(chaiHttp);
-
-// This agent refers to PORT where program is runninng.
-
-let server = supertest.agent("http://localhost:8000");
-let sequelize = require("sequelize");
-//let Books = require('../server/models/books');
-
-
-describe('GET Books',function(){
-
-  // #1 should return home page
-
-  it("should return books",function(done){
-
-    // calling home page api
-    server
-    .get('/api/v1/books')
-    .expect(200)
-        .end((err, response) => {
-          if (err) {
-            return done(err)
-          }
-          expect(response.body).toEqual()
-          return done()
-        })  });
-
+chai.use(require('chai-http'));
+ 
+const app = require('../app.js'); // Our app
+ 
+describe('API endpoint /signup', function() {  
+  this.timeout(1000); // How long to wait for a response (ms)
+ 
+  before(function() {
+ 
+  });
+ 
+  after(function() {
+ 
+  });
+ 
+  // POST - user details
+  it('should return the user details', function(req,res) {
+    return chai.request(app)
+      .post('/api/v1/users/signup')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.an('object');
+        expect(res.body.results).to.be.an('array');
+      });
+  });
+ 
+  // POST - Invalid path
+  it('should return Not Found', function(req,res) {
+    return chai.request(app)
+      .post('/INVALID_PATH')
+      .then(function(res) {
+        throw new Error('Not Found');
+      })
+      .catch(function(err) {
+        expect(err).to.have.status(404);
+      });
+  });
+ 
+  // POST - Login
+  it('should succesfully login user and generate a token for the user', function(req,res) {
+    return chai.request(app)
+      .post('/api/v1/users/signin')
+      .send({
+        email:'umeononihuadaobi@gmail.com' 
+      })
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.an('object');
+        expect(res.body.results).to.be.an('array').that.includes(
+          token);
+      });
+  });
+ 
+  // POST - Bad Request
+  it('should return Bad Request', function(req,res) {
+    return chai.request(app)
+      .post('/api/v1/users/signin')
+  
+      .send({
+        username: 'ada'
+      })
+      .then(function(res) {
+        throw new Error('Invalid content type!');
+      })
+      .catch(function(err) {
+        expect(err).to.have.status(400);
+      });
+  });
 });
+
+
 
