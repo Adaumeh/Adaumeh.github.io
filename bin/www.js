d8a3aa6f8515a2b6aa1fcb102a5dd427778aac70
umeononihu adaobi <adaumeh2003@yahoo.com>
2017-11-25 06:42:53 +0100

    modification on server file

--- bin/www.js
+++ bin/www.js
@@ -3,7 +3,7 @@ const http = require('http');
 const app = require('../app'); // The express app we just created
 
 //const port = parseInt(process.env.PORT, 10) || 8000;
-app.set('port', ( process.env.PORT || 8000 ));
+app.set('port', ( process.env.PORT || 3000 ));
 
 const server = http.createServer(app);
 server.listen( app.get( 'port' ), function() {
